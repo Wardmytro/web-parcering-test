da2105c28acc5bca7c379c0a9bd49c4f2d876581
Dmytro Ivanov <reek9136@gmail.com>
2026-05-20 22:49:00 +0300

    remove show_income

--- test.py
+++ test.py
@@ -1,7 +1,3 @@
-def show_income_tax_percentage(income = 1000, tax_percent = 0.05):
-    
-    print(income, tax_percent)
-
 def calculate_net_profit():
     income = float(input("Введіть ваш дохід: "))
     if income < 10000:
@@ -13,8 +9,4 @@ def calculate_net_profit():
 
 if __name__ == "__main__":
     calculate_net_profit()
-
-    show_income_tax_percentage()
-    show_income_tax_percentage(500,1)
-
-
+    
